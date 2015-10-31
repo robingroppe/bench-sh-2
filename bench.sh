7e9c8d2a78167fe5cddf369b91c7746e6a14b50f
Hidden Refuge <hidden-refuge@users.noreply.github.com>
2015-10-31 15:09:59 +0100

    Proper formatting + help page

--- bench.sh
+++ bench.sh
@@ -44,104 +44,116 @@ sysinfo () {
 	# Output of results
 	echo "System Info" | tee -a $HOME/bench.log
 	echo "-----------" | tee -a $HOME/bench.log
-	echo "Processor: $cname" | tee -a $HOME/bench.log
-	echo "CPU Cores: $cores" | tee -a $HOME/bench.log
-	echo "Frequency: $freq MHz" | tee -a $HOME/bench.log
-	echo "Memory: $tram MB" | tee -a $HOME/bench.log
-	echo "Swap: $vram MB" | tee -a $HOME/bench.log
-	echo "Uptime: $up" | tee -a $HOME/bench.log
+	echo "Processor	: $cname" | tee -a $HOME/bench.log
+	echo "CPU Cores	: $cores" | tee -a $HOME/bench.log
+	echo "Frequency	: $freq MHz" | tee -a $HOME/bench.log
+	echo "Memory		: $tram MB" | tee -a $HOME/bench.log
+	echo "Swap		: $vram MB" | tee -a $HOME/bench.log
+	echo "Uptime		: $up" | tee -a $HOME/bench.log
 	echo "" | tee -a $HOME/bench.log
-	echo "OS: $opsy" | tee -a $HOME/bench.log
-	echo "Arch: $arch ($lbit Bit)" | tee -a $HOME/bench.log
-	echo "Kernel: $kern" | tee -a $HOME/bench.log
-	echo "Hostname: $hn" | tee -a $HOME/bench.log
+	echo "OS		: $opsy" | tee -a $HOME/bench.log
+	echo "Arch		: $arch ($lbit Bit)" | tee -a $HOME/bench.log
+	echo "Kernel		: $kern" | tee -a $HOME/bench.log
+	echo "Hostname	: $hn" | tee -a $HOME/bench.log
 	echo "" | tee -a $HOME/bench.log
 	echo "" | tee -a $HOME/bench.log
 }
 speedtest4 () {
 	ipiv=$( wget -qO- ipv4.icanhazip.com ) # Getting IPv4
 	# Speed test via wget for IPv4 only with 10x 100 MB files. 1 GB bandwidth will be used!
-	echo "Speedtest IPv4 only" | tee -a $HOME/bench.log
-	echo "-------------------" | tee -a $HOME/bench.log
+	echo "Speedtest (IPv4 only)" | tee -a $HOME/bench.log
+	echo "---------------------" | tee -a $HOME/bench.log
 	echo "Your public IPv4 is $ipiv" | tee -a $HOME/bench.log
 	echo "" | tee -a $HOME/bench.log
 	# Cachefly CDN speed test
-	echo "CDN - nearest location:" | tee -a $HOME/bench.log
+	echo "Location		Provider	Speed"	| tee -a $HOME/bench.log
 	cachefly=$( wget -4 -O /dev/null http://cachefly.cachefly.net/100mb.test 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}' )
-	echo "Download speed from Cachefly: $cachefly " | tee -a $HOME/bench.log
+	echo "CDN			Cachefly	$cachefly" | tee -a $HOME/bench.log
 	echo "" | tee -a $HOME/bench.log
 	# United States speed test
-	echo "America - United States:" | tee -a $HOME/bench.log
 	coloatatl=$( wget -4 -O /dev/null http://speed.atl.coloat.com/100mb.test 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}' )
-	echo "Download speed from Coloat, Atlanta, GA: $coloatatl " | tee -a $HOME/bench.log
+	echo "Atlanta, GA, US		Coloat		$coloatatl " | tee -a $HOME/bench.log
 	sldltx=$( wget -4 -O /dev/null http://speedtest.dal05.softlayer.com/downloads/test100.zip 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}' )
-	echo "Download speed from Softlayer, Dallas, TX: $sldltx " | tee -a $HOME/bench.log
+	echo "Dallas, TX, US		Softlayer	$sldltx " | tee -a $HOME/bench.log
 	slwa=$( wget -4 -O /dev/null http://speedtest.sea01.softlayer.com/downloads/test100.zip 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}' )
-	echo "Download speed from Softlayer, Seattle, WA: $slwa " | tee -a $HOME/bench.log
+	echo "Seattle, WA, US		Softlayer	$slwa " | tee -a $HOME/bench.log
 	slsjc=$( wget -4 -O /dev/null http://speedtest.sjc01.softlayer.com/downloads/test100.zip 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}' )
-	echo "Download speed from Softlayer, San Jose, CA: $slsjc " | tee -a $HOME/bench.log
+	echo "San Jose, CA, US	Softlayer	$slsjc " | tee -a $HOME/bench.log
 	slwdc=$( wget -4 -O /dev/null http://speedtest.wdc01.softlayer.com/downloads/test100.zip 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}' )
-	echo "Download speed from Softlayer, Washington, DC: $slwdc " | tee -a $HOME/bench.log
+	echo "Washington, DC, US	Softlayer 	$slwdc " | tee -a $HOME/bench.log
 	echo "" | tee -a $HOME/bench.log
 	# Asia speed test
-	echo "Asia:" | tee -a $HOME/bench.log
 	linodejp=$( wget -4 -O /dev/null http://speedtest.tokyo.linode.com/100MB-tokyo.bin 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}' )
-	echo "Download speed from Linode, Tokyo, Japan: $linodejp " | tee -a $HOME/bench.log
+	echo "Tokyo, Japan		Linode		$linodejp " | tee -a $HOME/bench.log
 	slsg=$( wget -4 -O /dev/null http://speedtest.sng01.softlayer.com/downloads/test100.zip 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}' )
-	echo "Download speed from Softlayer, Singapore: $slsg " | tee -a $HOME/bench.log
-	echo "" | tee -a $HOME/bench.log 
+	echo "Singapore 		Softlayer	$slsg " | tee -a $HOME/bench.log
+	echo "" | tee -a $HOME/bench.log
 	# Europe speed test
-	echo "Europe:" | tee -a $HOME/bench.log
 	i3d=$( wget -4 -O /dev/null http://mirror.i3d.net/100mb.bin 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}' )
-	echo "Download speed from i3d.net, Rotterdam, Netherlands: $i3d" | tee -a $HOME/bench.log
+	echo "Rotterdam, Netherlands	id3.net		$i3d" | tee -a $HOME/bench.log
 	leaseweb=$( wget -4 -O /dev/null http://mirror.leaseweb.com/speedtest/100mb.bin 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}' )
-	echo "Download speed from Leaseweb, Haarlem, Netherlands: $leaseweb " | tee -a $HOME/bench.log
+	echo "Haarlem, Netherlands	Leaseweb	$leaseweb " | tee -a $HOME/bench.log
 	echo "" | tee -a $HOME/bench.log
 	echo "" | tee -a $HOME/bench.log
 }
 speedtest6 () {
 	ipvii=$( wget -qO- ipv6.icanhazip.com ) # Getting IPv6
   	# Speed test via wget for IPv6 only with 10x 100 MB files. 1 GB bandwidth will be used! No CDN - Cachefly not IPv6 ready...
-  	echo "Speedtest IPv6 only" | tee -a $HOME/bench.log
-  	echo "-------------------" | tee -a $HOME/bench.log
+  	echo "Speedtest (IPv6 only)" | tee -a $HOME/bench.log
+  	echo "---------------------" | tee -a $HOME/bench.log
   	echo "Your public IPv6 is $ipvii" | tee -a $HOME/bench.log
   	echo "" | tee -a $HOME/bench.log
+  	echo "Location		Provider	Speed" | tee -a $HOME/bench.log
   	# United States speed test
-  	echo "America - United States:" | tee -a $HOME/bench.log
 	v6atl=$( wget -6 -O /dev/null http://speedtest.atlanta.linode.com/100MB-atlanta.bin 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}' )
-	echo "Download speed from Linode, Atlanta, GA: $v6atl" | tee -a $HOME/bench.log
+	echo "Atlanta, GA, US		Linode		$v6atl" | tee -a $HOME/bench.log
   	v6dal=$( wget -6 -O /dev/null http://speedtest.dallas.linode.com/100MB-dallas.bin 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}' )
-  	echo "Download speed from Linode, Dallas, TX: $v6dal" | tee -a $HOME/bench.log
+  	echo "Dallas, TX, US		Linode		$v6dal" | tee -a $HOME/bench.log
   	v6new=$( wget -6 -O /dev/null http://speedtest.newark.linode.com/100MB-newark.bin 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}' )
-  	echo "Download speed from Linode, Newark, NJ: $v6new" | tee -a $HOME/bench.log
+  	echo "Newark, NJ, US		Linode	 	$v6new" | tee -a $HOME/bench.log
 	v6fre=$( wget -6 -O /dev/null http://speedtest.fremont.linode.com/100MB-fremont.bin 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}' )
-	echo "Download speed from Linode, Fremont, CA: $v6fre" | tee -a $HOME/bench.log
+	echo "Fremont, CA, US		Linode	 	$v6fre" | tee -a $HOME/bench.log
   	v6chi=$( wget -6 -O /dev/null http://testfile.chi.steadfast.net/data.bin 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}' )
-  	echo "Download speed from Steadfast, Chicago, IL: $v6chi" | tee -a $HOME/bench.log
+  	echo "Chicago, IL, US		Steadfast	$v6chi" | tee -a $HOME/bench.log
 	echo "" | tee -a $HOME/bench.log
 	# Asia speed test
-	echo "Asia:" | tee -a $HOME/bench.log
   	v6tok=$( wget -6 -O /dev/null http://speedtest.tokyo.linode.com/100MB-tokyo.bin 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}' )
-  	echo "Download speed from Linode, Tokyo, Japan: $v6tok" | tee -a $HOME/bench.log
+  	echo "Tokyo, Japan		Linode	 	$v6tok" | tee -a $HOME/bench.log
   	v6sin=$( wget -6 -O /dev/null http://speedtest.singapore.linode.com/100MB-singapore.bin 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}' )
-  	echo "Download speed from Linode, Singapore: $v6sin" | tee -a $HOME/bench.log
+  	echo "Singapore		Linode		$v6sin" | tee -a $HOME/bench.log
 	echo "" | tee -a $HOME/bench.log
 	# Europe speed test
-	echo "Europe:" | tee -a $HOME/bench.log
 	v6fra=$( wget -6 -O /dev/null http://speedtest.frankfurt.linode.com/100MB-frankfurt.bin 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}' )
-	echo "Download speed from Linode, Frankfurt, Germany: $v6fra" | tee -a $HOME/bench.log
+	echo "Frankfurt, Germany	Linode		$v6fra" | tee -a $HOME/bench.log
         v6lon=$( wget -6 -O /dev/null http://speedtest.london.linode.com/100MB-london.bin 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}' )
-	echo "Download speed from Linode, London, UK: $v6lon" | tee -a $HOME/bench.log
+	echo "London, UK		Linode		$v6lon" | tee -a $HOME/bench.log
         v6har=$( wget -6 -O /dev/null http://mirror.nl.leaseweb.net/speedtest/100mb.bin 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}' )
-        echo "Download speed from Leaseweb, Haarlem, Netherlands: $v6har" | tee -a $HOME/bench.log
+        echo "Haarlem, Netherlands	Leaseweb	$v6har" | tee -a $HOME/bench.log
 	echo "" | tee -a $HOME/bench.log
 	echo "" | tee -a $HOME/bench.log
 }
 iotest () {
+	echo "Disk Speed" | tee -a $HOME/bench.log
+	echo "----------" | tee -a $HOME/bench.log
 	# Measuring disk speed with DD
-	io=$( ( dd if=/dev/zero of=test_$$ bs=64k count=16k conv=fdatasync && rm -f test_$$ ) 2>&1 | awk -F, '{io=$NF} END { print io}' )
-	io2=$( ( dd if=/dev/zero of=test_$$ bs=64k count=16k conv=fdatasync && rm -f test_$$ ) 2>&1 | awk -F, '{io=$NF} END { print io}' )
-	io3=$( ( dd if=/dev/zero of=test_$$ bs=64k count=16k conv=fdatasync && rm -f test_$$ ) 2>&1 | awk -F, '{io=$NF} END { print io}' )
+	io=$( ( dd if=/dev/zero of=test_$$ bs=64k count=16k conv=fdatasync && rm -f test_$$ ) 2>&1 | awk -F, '{io=$NF} END { print io}' | sed 's/^[ \t]*//;s/[ \t]*$//' )
+	io2=$( ( dd if=/dev/zero of=test_$$ bs=64k count=16k conv=fdatasync && rm -f test_$$ ) 2>&1 | awk -F, '{io=$NF} END { print io}' | sed 's/^[ \t]*//;s/[ \t]*$//' )
+	io3=$( ( dd if=/dev/zero of=test_$$ bs=64k count=16k conv=fdatasync && rm -f test_$$ ) 2>&1 | awk -F, '{io=$NF} END { print io}' | sed 's/^[ \t]*//;s/[ \t]*$//' )
+	# Calculating avg I/O (better approach with awk for non int values)
+	ioraw=$( echo $io | awk 'NR==1 {print $1}' )
+	ioraw2=$( echo $io2 | awk 'NR==1 {print $1}' )
+	ioraw3=$( echo $io3 | awk 'NR==1 {print $1}' )
+	ioall=$( awk 'BEGIN{print '$ioraw' + '$ioraw2' + '$ioraw3'}' )
+	ioavg=$( awk 'BEGIN{print '$ioall'/3}' )
+	# Output of DD result
+	echo "I/O (1st run)	: $io" | tee -a $HOME/bench.log
+	echo "I/O (2nd run)	: $io2" | tee -a $HOME/bench.log
+	echo "I/O (3rd run)	: $io3" | tee -a $HOME/bench.log
+	echo "Average I/O	: $ioavg MB/s" | tee -a $HOME/bench.log
+	echo "" | tee -a $HOME/bench.log
+	echo "" | tee -a $HOME/bench.log
+	echo "IOPing Benchmark Results" | tee -a $HOME/bench.log
+	echo "------------------------" | tee -a $HOME/bench.log
 	# Measuring IOPing
 	wget --no-check-certificate -O $HOME/ioping https://hiddenrefuge.eu.org/stuff/ioping &> /dev/null
 	chmod +x $HOME/ioping
@@ -155,50 +167,45 @@ iotest () {
 	seqrad=$( $HOME/ioping -RLD . )
 	seqrac=$( $HOME/ioping -RLC . )
 	rm -rf $HOME/ioping
-	# Output of DD result
-	echo "Drive Speed:" | tee -a $HOME/bench.log
-	echo "------------" | tee -a $HOME/bench.log
-	echo "I/O (1st attempt): $io" | tee -a $HOME/bench.log
-	echo "I/O (2nd attempt): $io2" | tee -a $HOME/bench.log
-	echo "I/O (3rd attempt): $io3" | tee -a $HOME/bench.log
-	# Calculating avg I/O (better approach with awk for non int values)
-	ioraw=$( echo $io | awk 'NR==1 {print $1}' )
-	ioraw2=$( echo $io2 | awk 'NR==1 {print $1}' )
-	ioraw3=$( echo $io3 | awk 'NR==1 {print $1}' )
-	ioall=$( awk 'BEGIN{print '$ioraw' + '$ioraw2' + '$ioraw3'}' )
-	ioavg=$( awk 'BEGIN{print '$ioall'/3}' )
-	echo "Average I/O: $ioavg MB/s" | tee -a $HOME/bench.log
-	echo "" | tee -a $HOME/bench.log
 	# Output of IOPing
 	echo "" | tee -a $HOME/bench.log
-	echo "IOPing Results:" | tee -a $HOME/bench.log
-	echo "---------------" | tee -a $HOME/bench.log
 	echo "Latency (Async):" | tee -a $HOME/bench.log
 	echo "$lata" | tee -a $HOME/bench.log
+	echo "------------------------" | tee -a $HOME/bench.log
 	echo "" | tee -a $HOME/bench.log
 	echo "Latency (Direct):" | tee -a $HOME/bench.log
 	echo "$latd" | tee -a $HOME/bench.log
+	echo "------------------------" | tee -a $HOME/bench.log
 	echo "" | tee -a $HOME/bench.log
 	echo "Latency (Cache):" | tee -a $HOME/bench.log
 	echo "$latc" | tee -a $HOME/bench.log
+	echo "------------------------" | tee -a $HOME/bench.log
+	echo "" | tee -a $HOME/bench.log
 	echo "" | tee -a $HOME/bench.log
 	echo "Seek Rate (Async):" | tee -a $HOME/bench.log
 	echo "$seraa" | tee -a $HOME/bench.log
+	echo "------------------------" | tee -a $HOME/bench.log
 	echo "" | tee -a $HOME/bench.log
 	echo "Seek Rate (Direct):" | tee -a $HOME/bench.log
 	echo "$serad" | tee -a $HOME/bench.log
+	echo "------------------------" | tee -a $HOME/bench.log
 	echo "" | tee -a $HOME/bench.log
 	echo "Seek Rate (Cache):" | tee -a $HOME/bench.log
 	echo "$serac" | tee -a $HOME/bench.log
+	echo "------------------------" | tee -a $HOME/bench.log
+	echo "" | tee -a $HOME/bench.log
 	echo "" | tee -a $HOME/bench.log
 	echo "Sequential Speed (Async):" | tee -a $HOME/bench.log
 	echo "$seqraa" | tee -a $HOME/bench.log
+	echo "------------------------" | tee -a $HOME/bench.log
 	echo "" | tee -a $HOME/bench.log
 	echo "Sequential Speed (Direct):" | tee -a $HOME/bench.log
 	echo "$seqrad" | tee -a $HOME/bench.log
+	echo "------------------------" | tee -a $HOME/bench.log
 	echo "" | tee -a $HOME/bench.log
 	echo "Sequential Speed (Cache):" | tee -a $HOME/bench.log
 	echo "$seqrac" | tee -a $HOME/bench.log
+	echo "------------------------" | tee -a $HOME/bench.log
 	echo "" | tee -a $HOME/bench.log
 }
 gbench () {
@@ -208,8 +215,7 @@ gbench () {
 	echo "-------------------------------" | tee -a $HOME/bench.log
 	echo "" | tee -a $HOME/bench.log
 	echo "Note: The benchmark might not always work (eg: missing dependencies)." | tee -a $HOME/bench.log
-	echo "If it's too quick or takes too long it most likely means that the" | tee -a $HOME/bench.log
-	echo "benchmark has failed." | tee -a $HOME/bench.log
+	echo "Failures are highly possible. We're using Geekbench for this test." | tee -a $HOME/bench.log
 	echo "" | tee -a $HOME/bench.log
         gb_page=http://www.primatelabs.com/geekbench/download/linux/
         gb_dl=$(wget -qO - $gb_page | \
@@ -222,7 +228,7 @@ gbench () {
         wget -qO - "$gb_dl" | tar xzv 2>&1 >/dev/null
 	echo "" | tee -a $HOME/bench.log
 	echo "Starting $gb_name" | tee -a $HOME/bench.log
-	echo "The system benchmark with Geekbench may take a while." | tee -a $HOME/bench.log
+	echo "The system benchmark may take a while." | tee -a $HOME/bench.log
 	echo "Don't close your terminal/SSH session!" | tee -a $HOME/bench.log
 	echo "All output is redirected into a result file." | tee -a $HOME/bench.log
 	echo "" >> $HOME/bench.log
@@ -235,13 +241,35 @@ gbench () {
 	sleep 1
 	rm -rf $HOME/dist/
 	echo "" | tee -a $HOME/bench.log
-        gbl=$(sed -n '/following link/,/following link/ {/following link\|^$/b; p}' $HOME/bench.log)
+        gbl=$(sed -n '/following link/,/following link/ {/following link\|^$/b; p}' $HOME/bench.log | sed 's/^[ \t]*//;s/[ \t]*$//' )
 	echo "Benchmark Results: $gbl" | tee -a $HOME/bench.log
 	echo "Full report available at $HOME/bench.log" | tee -a $HOME/bench.log
 	echo "" | tee -a $HOME/bench.log
 }
+hlp () {
+	echo ""
+	echo "(C) Bench.sh 2 by Hidden Refuge <me at hiddenrefuge got eu dot org>"
+	echo ""
+	echo "Usage: bench.sh <option>"
+	echo ""
+	echo "Available options:"
+	echo "No option	: System information, IPv4 only speedtest and disk speed & IOPing benchmark will be run."
+	echo "-sys		: Displays system information such as CPU, amount CPU cores, RAM and more."
+	echo "-io		: Runs a disk speed test and a IOPing benchmark and displays the results."
+	echo "-6		: Normal benchmark but with a IPv6 only speedtest (run when you have IPv6)."
+	echo "-46		: Normal benchmark with IPv4 and IPv6 speedtest."
+	echo "-64		: Same as above."
+	echo "-b		: Normal benchmark with IPv4 only speedtest, I/O test and Geekbench system benchmark."
+	echo "-b6		: Normal benchmark with IPv6 only speedtest, I/O test and Geekbench system benchmark."
+	echo "-b46		: Normal benchmark with IPv4 and IPv6 speedtest, I/O test and Geekbench system benchmark."
+	echo "-b64		: Same as above."
+	echo "-h		: This help page."
+	echo ""
+	echo "The Geekbench system benchmark is experimental. So beware of failure!"
+	echo ""
+}
 case $1 in
-	'-sysinfo')
+	'-sys')
 		sysinfo;;
 	'-io')
 		iotest;;
@@ -249,17 +277,22 @@ case $1 in
 		sysinfo; speedtest6; iotest;;
 	'-46' )
 		sysinfo; speedtest4; speedtest6; iotest;;
+	'-64' )
+		sysinfo; speedtest4; speedtest6; iotest;;
 	'-b' )
 		sysinfo; speedtest4; iotest; gbench;;
 	'-b6' )
 		sysinfo; speedtest6; iotest; gbench;;
 	'-b46' )
 		sysinfo; speedtest4; speedtest6; iotest; gbench;;
+	'-b64' )
+		sysinfo; speedtest4; speedtest6; iotest; gbench;;
+	'-h' )
+		hlp;;
 	*)
 		sysinfo; speedtest4; iotest;;
 esac
 #################################################################################
-# Contributors									#
-#-------------------------------------------------------------------------------#
+# Contributors:									#
 # thirthy_speed https://freevps.us/thread-16943-post-191398.html#pid191398 	#
 #################################################################################
